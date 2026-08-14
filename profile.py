f9fe2a8d782dd5827d8af36e5a4a0bbc9fd1c5be
Soham Bagchi <soham.bagchi@utah.edu>
2026-08-14 12:19:35 -0600

    cloudlab profile

---
+++ profile.py
@@ -0,0 +1,17 @@
+import geni.portal as portal
+import geni.rspec.pg as pg
+
+# Create a Request object to start building the RSpec
+request = portal.context.makeRequestRSpec()
+
+# Request a single bare-metal node
+node = request.RawPC("node")
+
+# Force the node to be a d710 hardware type
+node.hardware_type = "d710"
+
+# Assign the standard Debian 13 image
+node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops//DEBIAN13-64-STD"
+
+# Print the RSpec to the portal
+portal.context.printRequestRSpec()
